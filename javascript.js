aea20707d7c46a176feb781a6e8acf8191a1cf9b
osamahCodes <osamahalghonaim@gmail.com>
2022-06-16 12:58:29 +0300

    Add functions for basic math operators

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(one, two){
+    return one+two;
+}
+
+function subtract(one,two){
+    return one-two;
+}
+
+function multiply(one,two){
+    return one*two;
+}
+
+function divide(one,two){
+    return one/two;
+}
